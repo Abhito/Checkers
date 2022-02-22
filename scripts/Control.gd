54dc9bbf3c87110f077b2197902efbe29a21b494
Ian Kerr <s22ian@test.com>
2022-02-22 04:03:35 -0500

    Prototyped AudioManager and SFX Example

---
+++ scripts/Control.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_Menu_pressed():
+	get_tree().change_scene("res://views/Menu.tscn")
+
+
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button_pressed():
+	visible = false
