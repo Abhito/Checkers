a66bf53495511d045edbb79ce738d81c3c21bffe
Joseph Minozzi <jminozzi@albany.edu>
2022-02-24 23:13:36 -0500

    Add saving to config functionality for some settings

--- scripts/ConfigController.gd
+++ scripts/ConfigController.gd
@@ -1,6 +1,11 @@
 extends Node
 
 onready var cameraFOV
+onready var local_player_one_name setget setLocalPlayerOneName, getLocalPlayerOneName
+onready var local_player_two_name
+onready var game_mode
+onready var display_mode
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,15 +14,18 @@ func _ready():
 	var config = ConfigFile.new()
 	config.load("res://userconfig.cfg")
 	var cameraFOV = config.get_value("Settings", "CameraFOV")
-	print(cameraFOV)
+	local_player_one_name = config.get_value("Settings", "local_player_one_name")
+	print(local_player_one_name)
+	#print(cameraFOV)
 
 #This is a stub where you update all settings from the values provided in the parameters
 func updateSettings():
-	print("This is a stub where you update all settings from the values provided in the parameters")
-	print(cameraFOV)
+	# print("This is a stub where you update all settings from the values provided in the parameters")
+	# print(cameraFOV)
 	var updateconfig = ConfigFile.new()
 	updateconfig.load("res://userconfig.cfg")
 	updateconfig.set_value("Settings", "CameraFOV", cameraFOV)
+	updateconfig.set_value("Settings", "local_player_one_name", local_player_one_name)
 	updateconfig.save("res://userconfig.cfg")
 	#Alternatively can just change this scruipts properties from another function like Controls in the form of ConfigController.cameraFov = 40
 
@@ -25,6 +33,8 @@ func updateSettings():
 func resetConfig():
 	var config = ConfigFile.new()
 	config.set_value("Settings", "CameraFOV", 90)
+	config.set_value("Settings", "local_player_one_name", "Player 1")
+	config.set_value("Settings", "local_player_two_name", "Player 2")
 	config.save("res://userconfig.cfg")
 
 #Should probably be made a global attribute getter
@@ -36,4 +46,23 @@ func setCameraFOV(value):
 	print(value)
 	cameraFOV = value
 	print(cameraFOV)
+	updateSettings()
+
+# Local Player 1 Name Getter
+func getLocalPlayerOneName():
+	#print(local_player_one_name)
+	return local_player_one_name
+
+# Local Player 1 Name Setter
+func setLocalPlayerOneName(value):
+	local_player_one_name = value
+	updateSettings()
+
+# Local Player 2 Name Getter
+func getLocalPlayerTwoName():
+	return local_player_two_name
+
+# Local Player 2 Name Setter
+func setLocalPlayerTwoName(value):
+	local_player_two_name = value
 	updateSettings()
--- scripts/Settings_Menu_Script.gd
+++ scripts/Settings_Menu_Script.gd
@@ -2,9 +2,11 @@ extends Control
 
 export (NodePath) var add_game_options_main_game_mode_options_dropdown_path
 export (NodePath) var add_game_options_video_display_mode_options_dropdown_path
+export (NodePath) var add_game_options_main_local_player_one_name_path
 
 onready var add_game_options_main_game_mode_options_dropdown = get_node(add_game_options_main_game_mode_options_dropdown_path)
 onready var add_game_options_video_display_mode_options_dropdown = get_node(add_game_options_video_display_mode_options_dropdown_path)
+onready var add_game_options_main_local_player_one_name = get_node(add_game_options_main_local_player_one_name_path)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +14,7 @@ func _ready():
 	# Add Display Mode Options
 	add_game_options_main_game_mode_options_dropdown()
 	add_game_options_video_display_mode_options_dropdown()
+	add_game_options_main_local_player_one_name()
 
 # Add Game Mode Options
 func add_game_options_main_game_mode_options_dropdown():
@@ -21,6 +24,12 @@ func add_game_options_main_game_mode_options_dropdown():
 func add_game_options_video_display_mode_options_dropdown():
 	add_game_options_video_display_mode_options_dropdown.add_item("Windowed")
 	add_game_options_video_display_mode_options_dropdown.add_item("Fullscreen")
+	
+# Add Local Player 1 Name to LineEdit
+func add_game_options_main_local_player_one_name():
+	var localPlayerOneName = ConfigController.getLocalPlayerOneName()
+	print(localPlayerOneName)
+	add_game_options_main_local_player_one_name.set_text(localPlayerOneName)
 
 #Quick return to menu button
 func _on_Return_pressed():
