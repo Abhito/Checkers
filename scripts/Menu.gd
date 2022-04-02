06be2adbb46dead650a97211ceb274f17ded970b
Ian Kerr <ikerr5555@gmail.com>
2022-04-02 15:10:37 -0400

    Added menu animation system for profile, overhauled login menu
    Former-commit-id: dd29e5654247a966c945e13380df3b73869a3413

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -3,11 +3,12 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var MenuAnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	AudioManager.stopMusic()
-
+	MenuAnimationPlayer = get_tree().get_root().get_node("Menu/MenuAnimator/AnimationPlayer")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -27,9 +28,8 @@ func _on_Options_pressed():
 func _on_Exit_pressed():
 	get_tree().quit()
 
-func _on_Button_pressed():
-	$AccountPanel.visible = false
-	$LoginPanel.visible = true
+func _on_AccountButton_pressed():
+	MenuAnimationPlayer.play("growLogin")
 
 func _on_RegisterButton_pressed():
 	$AccountPanel.visible = false
@@ -37,8 +37,7 @@ func _on_RegisterButton_pressed():
 	$RegisterPanel.visible = true
 
 func _on_LoginToProfile_pressed():
-	$AccountPanel.visible = true
-	$LoginPanel.visible = false
+	MenuAnimationPlayer.play("shrinkLogin")
 
 func _on_RegisterToProfile_pressed():
 	$AccountPanel.visible = true
