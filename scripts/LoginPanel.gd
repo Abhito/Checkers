ec7f0a401e1d3c8daede27f6e4dc3c198ed0fb8d
Ian Kerr <ikerr5555@gmail.com>
2022-04-02 17:23:05 -0400

    Added callback with userdata if verification is successful, and parsed data locally
    Former-commit-id: 0ce27be7de3c5cdba1bf38c2fb818473b133dc80

--- scripts/LoginPanel.gd
+++ scripts/LoginPanel.gd
@@ -2,14 +2,54 @@ extends ColorRect
 
 var username
 var password
+var tempHold
+var discoveredEmail
+var discoveredProfile
+signal verified
 
-signal resume
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
 
-func _on_HTTPRequestHandler_request_completed(result, response_code, headers, body):
-	pass # Replace with function body.
+func _ready():
+	tempHold = false
 
 func _on_LoginButton_pressed():
-	pass # Replace with function body.
+	username = $UsernameEntry.text
+	password = $PasswordEntry.text
+	var headers = ["Content-Type: application/json"]
+	if username != "" and password != "":
+		var verify = verifyPassword()
+		yield(self, "verified")
+		if tempHold == true:
+			print("Verification Successful")
+			$LoginRequestHandler.request("https://oh339unq37.execute-api.us-east-1.amazonaws.com/alpha/verification?user=" + username, headers, false, HTTPClient.METHOD_GET)
+		elif tempHold == false:
+			print("Verification Failed")
+	else:
+		print("Invalid Input")
+	
+func _on_LoginRequestHandler_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(typeof(json.result))
+	#print(json.result)
+	if typeof(json.result) == 18:
+		print("Dictionary Found")
+		print(json.result)
+		discoveredEmail = json.result.Items[0].email.S
+		discoveredProfile = json.result.Items[0].email.S
+	elif json != null:
+		if json.result == "false":
+			#print("False Found")
+			tempHold = false
+			emit_signal("verified")
+		elif json.result == null:
+			emit_signal("verified")
+		else:
+			print(json.result)
+	else:
+		pass
+
+func verifyPassword():
+	#Verify they match then send everything back but the password? Until we do a heftier implementation w/t auth tokens
+	#Also should move the HTTPS parameters to the query field so that they can be encoded
+	var headers = ["Content-Type: application/json"]
+	$LoginRequestHandler.request("https://oh339unq37.execute-api.us-east-1.amazonaws.com/alpha/verification?user=" + username + "&pass=" + password, headers, false, HTTPClient.METHOD_GET)
