c8c5125e91c84413bd3cabd1b44381f739db14f7
Ian Kerr <ikerr5555@gmail.com>
2022-04-02 14:18:48 -0400

    Small changes to login panel
    Former-commit-id: 4393586d3fcd49976e73bbca591093b749440a6c

---
+++ scripts/LoginPanel.gd
@@ -0,0 +1,15 @@
+extends ColorRect
+
+var username
+var password
+
+signal resume
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _on_HTTPRequestHandler_request_completed(result, response_code, headers, body):
+	pass # Replace with function body.
+
+func _on_LoginButton_pressed():
+	pass # Replace with function body.
