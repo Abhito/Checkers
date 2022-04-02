b4c63cb4d57de10682f7109ed38fa143ece402e4
Ian Kerr <ikerr5555@gmail.com>
2022-04-01 20:40:25 -0400

    Added a very ugly but functional profile/login interface mockup for testing
    Former-commit-id: 2883a1d46ca95b9d3150b92973c38c212b45ec08

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -26,3 +26,20 @@ func _on_Options_pressed():
 
 func _on_Exit_pressed():
 	get_tree().quit()
+
+func _on_Button_pressed():
+	$AccountPanel.visible = false
+	$LoginPanel.visible = true
+
+func _on_RegisterButton_pressed():
+	$AccountPanel.visible = false
+	$LoginPanel.visible = false
+	$RegisterPanel.visible = true
+
+func _on_LoginToProfile_pressed():
+	$AccountPanel.visible = true
+	$LoginPanel.visible = false
+
+func _on_RegisterToProfile_pressed():
+	$AccountPanel.visible = true
+	$RegisterPanel.visible = false
