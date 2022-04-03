e1671eb466c8e0b805d42359f57e461b879541b7
Ian Kerr <ikerr5555@gmail.com>
2022-04-03 14:25:06 -0400

    Added friend object, and friend list entry mockup before hooking into query
    Former-commit-id: e1f23f621ef10ba947c00656159a9a98c48ac8da

---
+++ scripts/AccountData.gd
@@ -0,0 +1,19 @@
+extends Object
+class_name AccountData
+
+var username
+var email
+var status
+var friendsList = Array()
+var friendRequests = Array()
+var profilePicture
+var settings
+var wins
+var losses
+
+func _init():
+	pass
+
+func updateAll(input):
+	pass
+
