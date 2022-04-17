60ed7067a5b593971709e356942d5e3ff26069d8
Ian Kerr <ikerr5555@gmail.com>
2022-04-16 20:59:03 -0400

    Added uploading profile pictures
    Former-commit-id: ef0e307c1b8a21ed61ac97396c38870c46889452

--- scripts/DetailedProfilePanel.gd
+++ scripts/DetailedProfilePanel.gd
@@ -27,3 +27,30 @@ func _on_SaveDescriptionButton_pressed():
 	get_node("SaveDescriptionButton").visible = false
 	get_node("ProfileDescription").visible = true
 	get_node("EditDescriptionButton").visible = true
+
+
+
+func _on_UploadPhoto_pressed():
+	$FileSelection.popup()
+
+
+func _on_FileSelection_file_selected(path):
+	var image = Image.new()
+	var err = image.load(path)
+	if err != OK:
+		print("Failed to open image.")
+	upload_image(image)
+	print("It made it here")
+	
+func upload_image(image):
+	image.resize(100, 100, 1)
+	var headers = ["Content-Type: application/json"]
+	#print(image.save_png_to_buffer())
+	var base64 = Marshalls.raw_to_base64(image.save_png_to_buffer())
+	var body = JSON.print({"imageName": AccountData.username, "image": base64, "type": "base64"})
+	#print(body)
+	$UploadProfilePicture.request("https://oh339unq37.execute-api.us-east-1.amazonaws.com/alpha/upload-photo", headers, false, HTTPClient.METHOD_PUT, body)
+
+func _on_UploadProfilePicture_request_completed(result, response_code, headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	print(response_code, " ", json.result)
