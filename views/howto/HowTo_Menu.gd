b6380e1e315e216b925c05aca4854e1617369eef
Joseph Minozzi <jminozzi@albany.edu>
2022-04-25 08:42:08 -0400

    Add Home button for How To Play
    Former-commit-id: faab9c7fce4c1be8584227641ad77b0931e7369e

--- views/howto/HowTo_Menu.gd
+++ views/howto/HowTo_Menu.gd
@@ -19,11 +19,11 @@ func _on_Previous_Button_pressed():
 func _on_Previous2_pressed():
 	get_tree().change_scene("res://views/howto/HowTo_Menu.tscn")
 
-
-
 func _on_Next2__pressed():
 	get_tree().change_scene("res://views/howto/HowTo_Menu3.tscn")
 
-
 func _on_Previous3_pressed():
 	get_tree().change_scene("res://views/howto/HowTo_Menu2.tscn")
+
+func _on_Previous3_Button2_pressed():
+	get_tree().change_scene("res://views/Menu.tscn")
